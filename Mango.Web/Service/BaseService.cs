8d78a1853b3f0df250b459319c7b2c2dd2bf20dd
duyquang2301 <duyquang.tdq7@gmail.com>
2024-10-19 00:01:39 +0700

    feat: api coupon and view

---
+++ Mango.Web/Service/BaseService.cs
@@ -0,0 +1,81 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Newtonsoft.Json;
+using System.Text;
+using static Mango.Web.Utils.SD;
+
+namespace Mango.Web.Service
+{
+    public class BaseService : IBaseService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BaseService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<ResponseDto?> SendAsync(RequestDto requestDto, bool withBearer = true)
+        {
+            try
+            {
+                HttpClient client = _httpClientFactory.CreateClient();
+                HttpRequestMessage message = new();
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(requestDto.Url);
+
+                if (requestDto.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(requestDto.Data), Encoding.UTF8, "application/json");
+                }
+
+                HttpResponseMessage? apiResponse = null;
+
+                switch (requestDto.ApiType)
+                {
+                    case ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                apiResponse = await client.SendAsync(message);
+
+                switch (apiResponse.StatusCode)
+                {
+                    case System.Net.HttpStatusCode.NotFound:
+                        return new ResponseDto { IsSuccess = false, Message = "Not Found" };
+                    case System.Net.HttpStatusCode.Unauthorized:
+                        return new ResponseDto { IsSuccess = false, Message = "Unauthorized" };
+                    case System.Net.HttpStatusCode.Forbidden:
+                        return new ResponseDto { IsSuccess = false, Message = "Access Denied" };
+                    case System.Net.HttpStatusCode.BadRequest:
+                        return new ResponseDto { IsSuccess = false, Message = "Bad Request" };
+                    case System.Net.HttpStatusCode.InternalServerError:
+                        return new ResponseDto { IsSuccess = false, Message = "Internal Server Error" };
+                    default:
+                        var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                        var responseDto = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
+                        return responseDto;
+                }
+            }
+            catch (Exception ex)
+            {
+                var dto = new ResponseDto
+                {
+                    IsSuccess = false,
+                    Message = ex.Message.ToString()
+                };
+                return dto;
+            }
+        }
+    }
+}
