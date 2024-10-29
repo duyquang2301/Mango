f43f8ba36836c363f26a00556917f96f66eb85da
duyquang2301 <duyquang.tdq7@gmail.com>
2024-10-29 22:15:14 +0700

    feat: add model entity and base api order

---
+++ Mango.Services.OrderAPI/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Mango.Services.OrderAPI.Models;
+using Mango.Services.OrderAPI.Models.Dto;
+
+namespace Mango.Services.OrderAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                
+            });
+            return mappingConfig;
+        }
+    }
+}
