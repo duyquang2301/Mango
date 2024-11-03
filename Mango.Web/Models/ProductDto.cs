7a3f7053ecce44bf3c954712b2a3e2197a316832
duyquang2301 <duyquang.tdq7@gmail.com>
2024-11-03 17:10:45 +0700

    feat: create api gateway

--- Mango.Web/Models/ProductDto.cs
+++ Mango.Web/Models/ProductDto.cs
@@ -10,11 +10,12 @@ namespace Mango.Web.Models
         public double Price { get; set; }
         public string Description { get; set; }
         public string CategoryName { get; set; }
-        public string ImageUrl { get; set; }
-        [Range(1,1000)]
+        public string? ImageUrl { get; set; }
+        public string? ImageLocalPath { get; set; }
+        [Range(1, 100)]
         public int Count { get; set; } = 1;
         [MaxFileSize(1)]
         [AllowedExtensions(new string[] { ".jpg", ".png" })]
-        public IFormFile Image { get; set; }
+        public IFormFile? Image { get; set; }
     }
 }
--- Mango.Web/Utils/AllowedExtensionsAttribute.cs
+++ Mango.Web/Utils/AllowedExtensionsAttribute.cs
@@ -2,26 +2,29 @@
 
 namespace Mango.Web.Utility
 {
-    public class AllowedExtensionsAttribute: ValidationAttribute
+    public class AllowedExtensionsAttribute : ValidationAttribute
     {
         private readonly string[] _extensions;
-        public AllowedExtensionsAttribute(string[] extentions)
+        public AllowedExtensionsAttribute(string[] extensions)
         {
-            _extensions = extentions;
+            _extensions = extensions;
         }
 
         protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
         {
             var file = value as IFormFile;
-            if(file != null)
+
+            if (file != null)
             {
                 var extension = Path.GetExtension(file.FileName);
                 if (!_extensions.Contains(extension.ToLower()))
                 {
-                    return new ValidationResult($"This extension is not allowed!");
+                    return new ValidationResult("This photo extension is not allowed!");
                 }
             }
-            return base.IsValid(value, validationContext);
+
+            return ValidationResult.Success;
         }
+
     }
 }
